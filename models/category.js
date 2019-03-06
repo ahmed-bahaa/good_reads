76a8a6201615ed2edef42269babb7998077efa49
nuranlamei <lamei.nouran@gmail.com>
2019-03-06 23:26:55 +0200

    pages: category and category_update_form added and linked. category list done, while update has a bug

--- models/category.js
+++ models/category.js
@@ -7,5 +7,5 @@ const category_schema = new mongoose.Schema({
 });
 
 const category_model = mongoose.model("category", category_schema)
-
+//category_model.create({name:""})
 module.exports = category_model;
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,11 +1,51 @@
 const express = require("express")
 const admin_router = express.Router()
-//const author_model = require('../models/author');
+const category_model = require('../models/category');
 
 //======================== GET ==========================
+// lists categories
+admin_router.get('/category/', async (req,res)=>{
+    try
+    {
+        const categories = await category_model.find({})
+       // res.send(categories)
+        res.render('../views/pages/category.ejs', {categories:categories,})
+    }
+    catch(e){
+        console.log(e)
+    }
+})
 
+//redirects from update button
+admin_router.get('/category/:id/edit', async (req,res)=>{
+
+    try{
+       
+        res.render('../views/pages/category_update_form.ejs')
+
+    }
+    catch(e){
+        console.log(e)
+    }
+
+})
 
 //======================== POST =========================
+
+
+//redirects from update button
+admin_router.post('/category/:id/edit', async (req,res)=>{
+
+    try{
+        const updated_category = await category_model.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        const categories = await category_model.find({})
+        res.render('../views/pages/category.ejs', {categories:categories,})
+    }
+    catch(e){
+        console.log(e)
+    }
+
+})
 
 
 //======================== DELETE =======================
