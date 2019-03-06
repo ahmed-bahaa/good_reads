5cf5ef1278f0af3095e1bb550ba982583eab7f78
nuranlamei <lamei.nouran@gmail.com>
2019-03-06 20:02:35 +0200

    routes: added empty route . js files [adminRoute , userRoute]

---
+++ routes/adminRouter.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const admin_router = express.Router()
+//const author_model = require('../models/author');
+
+//======================== GET ==========================
+
+
+//======================== POST =========================
+
+
+//======================== DELETE =======================
+
+
+//======================== PUT ==========================
+
+module.exports = admin_router
