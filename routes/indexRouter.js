e85534f5b74a91f25d43cb4aa80dfc21bc0b12cd
hagar <hagar@localhost.localdomain>
2019-03-07 22:56:38 +0200

    publishing popular items

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,9 +7,9 @@ const book = require('../models/book')
 index_router.get('/index',async(req,res,)=>{
     try
     {
-        const authors = await author.find({})
-        const books = await author.find({})
-        const categories = await category.find({})
+        const authors = await author.find({}).find({}).sort({name:1}).limit(3) //replace name with sth else
+        const books = await book.find({}).find({}).sort({name:1}).limit(3) //replace name with sth else
+        const categories = await category.find({}).sort({name:1}).limit(3) //replace name with sth else
         res.render('../views/pages/index.ejs',{categories:categories, books:books, authors:authors})
     }
     catch(e)
