24eecb9794c2bb5f7ca6717ad1ef06f6d077e7c8
abahaa <abahaa@localhost.localdomain>
2019-03-07 13:00:48 +0200

    packages file included

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const category_model = require('../models/category.js')
 const admin_router = express.Router()
 //const author_model = require('../models/author');
 
@@ -7,9 +8,32 @@ const admin_router = express.Router()
 
 //======================== POST =========================
 
+admin_router.post( '/category/', async(req,res)=>{
+    try
+    {
+        //console.log(req.body.fname)
+        const new_category = await category_model.create({ name:req.body.name})
+        const categories = await category_model.find({}) 
+        res.json(categories);
+    }
+    catch( err ){
+        console.log( err )
+    }})
+
 
 //======================== DELETE =======================
 
+admin_router.get( '/category/:id/delete', async(req,res)=>{
+    try
+    {      
+        console.log(req.params.id)
+        const deleted_book = await category_model.findByIdAndRemove(req.params.id.replace(":",""))
+        const books = await category_model.find({}) 
+        res.json(books);
+    }
+    catch( err ){
+        console.log( err )
+    }})
 
 //======================== PUT ==========================
 
