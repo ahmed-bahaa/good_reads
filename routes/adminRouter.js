4d69aefe0149378730f0044b9a54fa4c424c2c92
abahaa <abahaa@localhost.localdomain>
2019-03-09 20:04:46 +0200

    book handlers done

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -10,6 +10,42 @@ const categ_router = require("./adminCategory")
 admin_router.use('/category/', categ_router)
 
 
+//redirects from update button
+admin_router.get('/book/:id/edit', async (req,res)=>{
+    try{
+        console.log("yeaaah")
+        
+        const book = await book_model.findById(req.params.id.replace(":","")).populate("author_id").populate("category_id")
+        
+        const available_categories = await category_model.find({})
+        const available_authors = await author_model.find({})
+        res.render('../views/pages/admin/book_form.ejs',{book:book,authores:available_authors,categories:available_categories})
+        }
+    catch(e){
+        console.log(e)
+    }
+
+})
+
+
+//redirects from update form
+admin_router.post('/book/:id/edit', async (req,res)=>{
+
+   
+    try{
+        console.log("yeaaah")
+        console.log(req.body)
+        const updated_book = await book_model.findByIdAndUpdate(req.params.id.replace(":",""), req.body, {new: true})
+        const books = await book_model.find({}).populate("author_id").populate("category_id")
+        res.render('../views/pages/admin/book.ejs', {books:books,})
+        }
+    catch(e){
+        console.log(e)
+    }
+
+})
+
+
 //======================== book ==========================
 
 admin_router.get( '/book/new', async (req,res)=>{
@@ -23,17 +59,32 @@ admin_router.get( '/book/new', async (req,res)=>{
     catch(e){
     console.log(e)
 }})
+
+//======================== DELETE Book=======================
+
+admin_router.get( '/book/:id/delete', async(req,res)=>{
+    try
+    {      
+        console.log(req.params.id)
+        const deleted_book = await book_model.findByIdAndRemove(req.params.id.replace(":",""))
+        const books = await book_model.find({}) 
+
+        res.render('../views/pages/admin/book.ejs', {books:books,})
+    }
+    catch( err ){
+        console.log( err )
+    }})
 //======================== POST =========================
 
 admin_router.post( '/book/:id/add', async(req,res)=>{
     try
     {
-        const new_category = await category_model.create({ 
-            name:req.body.name,
-            cover:req.body.cover,
-            description:req.body.description,
-            author_id:req.body.author,
-            category_id:req.body.category})
+        const new_book = await book_model.create({ 
+                        name:req.body.name,
+                        cover:req.body.cover,
+                        description:req.body.description,
+                        author_id:req.body.author,
+                        category_id:req.body.category})
         const books = await book_model.find({}).populate("author_id").populate("category_id")
         // res.send(books[0])
         res.render('../views/pages/admin/book.ejs', {books:books,})
@@ -47,15 +98,17 @@ admin_router.post( '/book/:id/add', async(req,res)=>{
 admin_router.get('/book', async (req,res)=>{
     try
     {
-        const books = await book_model.find({}).populate({path:'category_id' ,select:"name"}).populate({ path:'author_id',select:["fname","lname"]})
+        const books = await book_model.find({}).populate("author_id").populate("category_id")
        // res.send(categories)
-        res.send(books)
+        res.render('../views/pages/admin/book.ejs', {books:books,})
     }
     catch(e){
         console.log(e)
     }
 })
-
+//========================================================
+//========================================================
+//========================================================
 //======================== GET ==========================
 
 
