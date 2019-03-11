096a7869a9739525d6b7cb71c670cfd1a21e0cf1
hagar <hagar@localhost.localdomain>
2019-03-11 20:27:55 +0200

    updating indexrouter most popular section into json objects

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -10,10 +10,11 @@ const category_model = require('../models/category.js');
 index_router.get('/index',async(req,res,)=>{
     try
     {
-        const authors = await author.find({}).find({}).sort({name:1}).limit(3) //replace name with sth else
-        const books = await book.find({}).find({}).sort({name:1}).limit(3) //replace name with sth else
-        const categories = await category.find({}).sort({name:1}).limit(3) //replace name with sth else
-        res.render('../views/pages/index.ejs',{categories:categories, books:books, authors:authors})
+        const authors = await author.find({}).select('fname -_id').sort({fname:1}).limit(3) //replace name with sth else
+        const books = await book.find().select('name -_id').sort({name:1}).limit(3) //replace name with sth else
+        const categories = await category.find().select('name -_id').sort({name:1}).limit(3) //replace name with sth else
+        res.json({categories:categories, books:books, authors:authors})
+        //res.render('../views/pages/index.ejs',{categories:categories, books:books, authors:authors})
     }
     catch(e)
     {
